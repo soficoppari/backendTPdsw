4395874f4a405520dac986ce1a76382115e8dbf8
octaviodobrovits <octaviodobrovits@gmail.com>
2024-08-16 18:01:42 -0300

    acceso a bd parte 1

--- src/shared/repository.ts
+++ src/shared/repository.ts
@@ -1,8 +1,8 @@
 export interface Repository<T> {
-  findAll(): T[] | undefined
-  findOne(mascotas: {id: string; fechaI: Date; fechaF:Date}): T | undefined  //modificable el mascotas: id string
-  add (mascotas: T): T | undefined
-  update(mascotas: T): T | undefined
-  delete(mascotas: {id: string; fechaI:Date ; fechaF:Date}): T | undefined
+  findAll(): Promise<T[] | undefined>
+  findOne(mascotas: {id: string; fechaI: Date; fechaF:Date}): Promise<T | undefined>  //modificable el mascotas: id string
+  add (mascotas: T): Promise<T | undefined>
+  update(mascotas: T): Promise<T | undefined>
+  delete(mascotas: {id: string; fechaI:Date ; fechaF:Date}): Promise<T | undefined>
   
 }
